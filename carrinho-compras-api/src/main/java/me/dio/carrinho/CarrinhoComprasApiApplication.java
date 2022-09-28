bf1e54036a376e082d9647062fec23da6898483a
Nicolas Maulaiz <nnicolasmaulaiz@gmail.com>
2022-09-28 11:32:11 -0300

    28-09-2022

---
+++ carrinho-compras-api/src/main/java/me/dio/carrinho/CarrinhoComprasApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.carrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoComprasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoComprasApiApplication.class, args);
+	}
+
+}
